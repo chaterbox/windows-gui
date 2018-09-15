f137c38ea94ced0f30995e3605de1e2bae293bab
chaterbox <chatbox8558@gmail.com>
2018-09-14 19:36:08 -0500

    pushing latest update

--- gui/main.cpp
+++ gui/main.cpp
@@ -14,6 +14,7 @@ HMENU hMenu;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInst, LPSTR args, int ncmdshow)
 {
+	//window class
 	WNDCLASSW wc = { 0 };
 
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
@@ -36,7 +37,6 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInst, LPSTR args, int ncmdsho
 	}
 	return 0;
 }
-
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg)
@@ -44,20 +44,25 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 	case WM_COMMAND:
 		switch (wp)
 		{
+		//appends close program to a menu button
 		case FILE_MENU_EXIT:
-			DestroyWindow(hWnd);
+				DestroyWindow(hWnd);
 			break;
+		//makes file ment new make a sound
 		case FILE_MENU_NEW:
-			MessageBeep(MB_OK);
+				MessageBeep(MB_OK);
 			break;
+		//makes a messagebox to connect me
 		case HELP_MENU:
-			MessageBox(hWnd,"     to contact me email \n chatbox8558@gmail.com", "help", MB_OK);
+				MessageBox(hWnd,"     to contact me email \n chatbox8558@gmail.com", "help", MB_OK);
 			break;
 		}
 		break;
+	//creates menu
 	case WM_CREATE:
 		AddMenus(hWnd);
 		break;
+	//closes program
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
@@ -69,9 +74,8 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 void AddMenus(HWND hWnd)
 {
 	hMenu = CreateMenu();
-	HMENU hFileMenu = CreateMenu();
-	HMENU hHelpMenu = CreateMenu();
 	//creates the file menu
+	HMENU hFileMenu = CreateMenu();
 	AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, "file");
 	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NEW, "new file");
 	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_OPEN, "open file");
@@ -80,6 +84,7 @@ void AddMenus(HWND hWnd)
 	AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
 	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, "exit");
 	//creates the help menu
+	HMENU hHelpMenu = CreateMenu();
 	AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hHelpMenu, "help");
 	AppendMenu(hHelpMenu, MF_STRING, HELP_MENU, "contact");
 	SetMenu(hWnd, hMenu);
